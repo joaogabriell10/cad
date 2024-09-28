f181a9c6f026cb5d8b1472b495bf9982f96ba922
programadormovel <programadormovel@gmail.com>
2024-09-28 18:09:15 -0300

    deletar

--- src/pages/Listagem.jsx
+++ src/pages/Listagem.jsx
@@ -1,14 +1,19 @@
 import React, {useState, useEffect} from 'react';
+import './Listagem.css';
 import { Formik } from 'formik';
 import axios from 'axios';
 import { Link } from 'react-router-dom'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import logo from '../assets/react.svg'
+import logo from '../assets/react.svg';
+import lixo from '../assets/delete.png';
+import trash from '../assets/trash.svg';
+
 
 const Listagem = () => {
 
     const [dados, setDados] = useState([])
+    const [itemApagado, setItemApagado] = useState(false)
 
     function receberDados(){
         axios.get('http://localhost:8080/produto'
@@ -18,30 +23,60 @@ const Listagem = () => {
         })
         .catch(error => console.log(error))
     }
+
+    async function apagarDados(produto){
+        axios.delete('http://localhost:8080/produto',
+        {
+            data : produto,
+            headers: {                  
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "Origin, Content-Type, Accept, Authorization", 
+                "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE" ,
+                "Content-Type": "application/json;charset=UTF-8"                   
+            },
+        })
+        .then(response => {
+            console.log(response)
+            console.log('Dados apagados!')
+            setItemApagado(true)
+        })
+        .catch(error => console.log(error))
+    }
     
     useEffect(()=>{
-       receberDados()
+        receberDados()
     }, [])
+
+    useEffect(()=>{
+        if(itemApagado)
+            receberDados()
+        return() =>{
+            setItemApagado(false)
+        }
+    }, [itemApagado])
 
     const ItensLista = () => dados.map(
         produto => 
-        <li key={produto.id} style={{listStyle: 'none'}}>
-            <Card style={{ width: '18rem', borderRadius:8, 
-                            margin: 16,  
-                            backgroundColor:'pink' }}>
-            <Card.Img variant="top" src={logo} />
-            <Card.Body>
-                <Card.Title>{produto.nome}</Card.Title>
-                <Card.Text>
-                    {produto.descricao}
-                </Card.Text>
-                <Card.Text>
-                    {produto.preco}
-                </Card.Text>
-                <Link to="/detalhe" state={{produto: produto}}>
-                    Detalhe do Produto
-                </Link>
-            </Card.Body>
+        <li key={produto.id} >
+            <Card className="card-product">
+                <Card.Img className="garbage"
+                    onClick={async ()=>{
+                        await apagarDados(JSON.stringify(produto))
+                    }} 
+                    src={trash} />
+                <Card.Img className="img-product" variant="top" src={logo} />
+                <Card.Body>
+                    <Card.Title>{produto.nome}</Card.Title>
+                    <Card.Text>
+                        {produto.descricao}
+                    </Card.Text>
+                    <Card.Text>
+                        {produto.preco}
+                    </Card.Text>
+                    <Link to="/detalhe" state={{produto: produto}}>
+                        Detalhe do Produto
+                    </Link>
+                </Card.Body>
             </Card>
         </li>
     )
@@ -54,7 +89,7 @@ const Listagem = () => {
             CADASTRO
         </Link>
 
-        <ul>
+        <ul className="row">
             <ItensLista />
         </ul>
 
